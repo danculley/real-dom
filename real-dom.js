0491c5cec6158ac33573f85580c6246999478012
danculley <danculley@gmail.com>
2016-12-28 18:49:13 -0500

    Update real-dom.js

--- real-dom.js
+++ real-dom.js
@@ -4,7 +4,6 @@ const maybe = (...funcs) => funcs.reduce((accumulator, current) => arg => {
     ? null
     : current(result)
 }, arg => arg)
-    , flatten = array => array.reduce((acc, cur) => acc.concat(cur), [])
 
 const removeAllChildren = element => (Array.prototype.forEach.call(element.children, child => element.removeChild(child)), element)
     , returnFocusToElement = maybe(x => x.id, x => document.getElementById(x), x => x.focus())
@@ -20,17 +19,19 @@ export const h = (tagName, props = {}, ...children) => {
     return tagName(props)
   const node = Object.assign(document.createElement(tagName), props)
   Object.assign(node.style, (props || {}).style)
-  flatten(children).forEach(child => node.appendChild((typeof child !== 'object') ? document.createTextNode(child) : child))
+  children
+    .reduce((acc, cur) => acc.concat(cur), [])
+    .forEach(child => node.appendChild((typeof child !== 'object') ? document.createTextNode(child) : child))
   return node
 }
 
 export const component = (initState, view, reducer = (state, action) => action, registerSubscriptions = dispatch => null) => root => {
   let state = initState
   const renderer = render(root)
-  const dispatch = action => {
+  const dispatch = action => Promise.resolve(action).then(action => {
     state = reducer(state, action)
     renderer(view(state, dispatch))
-  }
+  })
   registerSubscriptions(dispatch)
   renderer(view(state, dispatch))
 }
