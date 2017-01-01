7ecbe7beb66ebcce4b2f4152862df90c903ac770
danculley <danculley@gmail.com>
2017-01-01 15:27:23 -0500

    Update real-dom.js

--- real-dom.js
+++ real-dom.js
@@ -1,37 +1,34 @@
 const maybe = (...funcs) => funcs.reduce((accumulator, current) => arg => {
-  const result = accumulator(arg)
-  return (result === null || result === undefined)
-    ? null
-    : current(result)
-}, arg => arg)
-
-const removeAllChildren = element => (Array.prototype.forEach.call(element.children, child => element.removeChild(child)), element)
-    , returnFocusToElement = maybe(x => x.id, x => document.getElementById(x), x => x.focus())
-
+  const result = accumulator(arg);
+  return (result === null || result === undefined) ? null : current(result);
+}, arg => arg);
+const removeAllChildren = element => (Array.prototype.forEach.call(element.children, child => element.removeChild(child)), element);
+const returnFocusToElement = maybe(x => x.id, x => document.getElementById(x), x => x.focus());
 const render = root => tree => {  
-  const focused = document.activeElement
-  removeAllChildren(root).appendChild(tree)
-  returnFocusToElement(focused)
-}
+  const focused = document.activeElement;
+  removeAllChildren(root).appendChild(tree);
+  returnFocusToElement(focused);
+};
 
 export const h = (tagName, props, ...children) => {
-  if (typeof tagName === 'function')
-    return tagName(props)
-  const node = Object.assign(document.createElement(tagName), props)
-  Object.assign(node.style, (props || {}).style)
+  if (typeof tagName === 'function') return tagName(props);
+  
+  const node = Object.assign(document.createElement(tagName), props);
+  Object.assign(node.style, (props || {}).style);
   children
     .reduce((acc, cur) => acc.concat(cur), [])
-    .forEach(child => node.appendChild((typeof child !== 'object') ? document.createTextNode(child) : child))
-  return node
-}
+    .forEach(child => node.appendChild((typeof child !== 'object') ? document.createTextNode(child) : child));
+  
+  return node;
+};
 
 export const component = (initState, view, reducer = (state, action) => action, registerSubscriptions = dispatch => null) => root => {
-  let state = initState
-  const renderer = render(root)
+  let state = initState;
+  const renderer = render(root);
   const dispatch = action => Promise.resolve(action).then(action => {
-    state = reducer(state, action)
-    renderer(view(state, dispatch))
-  })
-  registerSubscriptions(dispatch)
-  renderer(view(state, dispatch))
-}
+    state = reducer(state, action);
+    renderer(view(state, dispatch));
+  });
+  registerSubscriptions(dispatch);
+  renderer(view(state, dispatch));
+};
